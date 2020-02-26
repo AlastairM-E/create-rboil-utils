033da0734686d4fe4c82d2d4cb3f14273cc9d861
AlastairM-E <alastairunityemail@gmail.com>
2020-02-26 22:27:37 +0000

    still need to develop the thing better. Needs work.

--- cli-utils/testFilesOnly.js
+++ cli-utils/testFilesOnly.js
@@ -2,7 +2,7 @@ function testFilesOnly(dir) {
      return dir.reduce((accumulator, file, index) => {
 
         if (typeof file === 'object' && file !== Array) {
-           accumulator.push(...testFilesOnly(file.subFiles));
+           accumulator.push(`${dir}/${testFilesOnly(file.subFiles).flat(Infinity)}`);
 
         } else if (file.match(/.*\.test\.js/)) {
             accumulator.push(file);
