3d0e20b50b21a80379f0f8004a3c43c62c692d43
AlastairM-E <alastairunityemail@gmail.com>
2020-02-27 21:49:23 +0000

    This is current incorrect, but more structure need to make an clean testing system. For now, it is better to jsut clear up the mess and have a botched annoying version.

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const { readSubFilesFrom } = require('./cli-utils/readSubFilesFrom');
 const { testFilesOnly } = require('./cli-utils/testFilesOnly');
 const { outputDirContentOf } = require('./cli-utils/outputDirContentOf');
 
+const path = require('path');
+
+console.log(testFilesOnly(
+    readSubFilesFrom(path.join(__dirname, '/src'))
+));
+
 module.exports = {
     readSubFilesFrom,
     testFilesOnly,
